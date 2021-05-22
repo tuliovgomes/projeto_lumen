6716633440a754b39b8ac3374106a29cfcd428b5
Túlio Gomes <tuliogomes07@hotmail.com>
2021-05-22 11:35:11 -0300

    atualizando factory de usuario

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
@@ -22,8 +23,9 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name'     => $this->faker->name,
+            'email'    => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('123456'),
         ];
     }
 }
