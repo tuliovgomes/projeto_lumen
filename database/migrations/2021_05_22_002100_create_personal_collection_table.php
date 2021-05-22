d26a474bd8c64f997e4550f313eea3a36b1ec56b
Túlio Gomes <tuliogomes07@hotmail.com>
2021-05-21 21:28:08 -0300

    adicionando tabela de coleção pessoal

---
+++ database/migrations/2021_05_22_002100_create_personal_collection_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalCollectionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_collection', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('type');
+            $table->unsignedBigInteger('contacts_id');
+
+            $table->foreign('contacts_id')->references('id')->on('contacts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_collection');
+    }
+}
